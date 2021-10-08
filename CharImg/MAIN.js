ae3f508aaa51f841af8000d2f25bccf7ef9d2a8b
Sean P. Myrick V19.1.7.2 <65933340+seanpm2001@users.noreply.github.com>
2021-10-08 16:40:02 -0700

    MAIN.js
    The main script for this directory.

---
+++ CharImg/MAIN.js
@@ -0,0 +1,14 @@
+// Start of script
+/*Character image library index */
+class charImgResearch_database(); { // I don't know where to go with this
+  console.log("GOTO /CharImg/LIBRARY/001/Characters/");
+  file.Path('/CharImg/LIBRARY/001/Characters/');
+  break;
+  }
+}
+/* File info
+* File version: 1 (Friday, 2021 October 8th at 4:40 pm)
+* File type: JavaScript source file (*.js)
+* Line count (including blank lines and compiler line): 15
+*/
+// End of script
